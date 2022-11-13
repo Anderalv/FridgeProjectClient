a95f25080b2aa90bd2ec55badfe3953a06803af4
Anderalv <andreylastovski@gmail.com>
2022-11-13 18:52:01 +0300

    added visual part

--- FridgeProject/Controllers/HomeController.cs
+++ FridgeProject/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FridgeProject.Models;
@@ -22,11 +18,35 @@ namespace FridgeProject.Controllers
         {
             return View();
         }
-
+        public IActionResult AllFridges()
+        {
+            return View();
+        }
+        public IActionResult AllProducts()
+        {
+            return View();
+        }
+        public IActionResult CreateFridge()
+        {
+            return View();
+        } 
+        public IActionResult EditFridge()
+        {
+            return View();
+        }
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+        public IActionResult EditProduct()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
